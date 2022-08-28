3cb30eb9f78022f4a06c360c5b46b10d5064f489
SilasAggrey <91203655+SilasAggrey@users.noreply.github.com>
2022-08-28 21:13:46 +0100

    update

--- src/components/Registeration/index.js
+++ src/components/Registeration/index.js
@@ -318,9 +318,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   formlable: {
-    // fontSize: 18,
-    // alignSelf: "center",
-    // marginHorizontal: 10,
     marginVertical: 5,
     fontSize: 14,
     color: COLORS.grey,
